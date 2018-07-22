c993d9895aae3908e9a71825471b69c37cf67be8
Christopher Dennis <cdennis.aus@gmail.com>
2018-07-22 18:42:07 -0500

    Added login route to users API

--- routes/api/users.js
+++ routes/api/users.js
@@ -43,4 +43,27 @@ router.post('/register', (req, res) => {
     });
 });
 
+// @route  GET api/users/login
+// @desc   Login user / Returning Token
+// @access Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({email})
+    .then(user => {
+      if (!user) return res.status(404).json({email: 'User not found'});
+
+      bcrypt.compare(password, user.password)
+        .then(matched => {
+          if (matched) {
+            res.json({msg: 'Success'});
+          } else{
+            return res.status(400).json({password: 'Password incorrect'});
+          }
+        });
+    });
+});
+
 module.exports = router;
