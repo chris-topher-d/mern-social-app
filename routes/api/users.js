4764059587e7595710b80aee44a1962e12caea58
Christopher Dennis <cdennis.aus@gmail.com>
2018-07-23 01:29:55 -0500

    Added name validation for registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+// Load input validation
+const validateRegisterInput = require('../../validation/register');
+
 // User model
 const User = require('../../models/User');
 
@@ -20,6 +23,11 @@ router.get('/test', (req, res) => {
 // @desc   Register user
 // @access Public
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) return res.status(400).json(errors);
+
   User.findOne({ email: req.body.email })
     .then(user => {
       if (user) {
