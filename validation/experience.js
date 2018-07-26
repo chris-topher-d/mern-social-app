2fe6f81ef5d6c6de8cf28556badd502d4438af04
Christopher Dennis <cdennis.aus@gmail.com>
2018-07-25 20:41:51 -0500

    Added experience data validation

---
+++ validation/experience.js
@@ -0,0 +1,30 @@
+const Validator = require('validator');
+const isEmpty = require('./isEmpty');
+
+module.exports = validateExperienceInput = (data) => {
+  let errors = {};
+
+  data.title = !isEmpty(data.title) ? data.title : '';
+  data.company = !isEmpty(data.company) ? data.company : '';
+  data.from = !isEmpty(data.from) ? data.from : '';
+
+  // Validate title
+  if (Validator.isEmpty(data.title)) {
+    errors.title = 'Job title is required';
+  }
+
+  // Validate company
+  if (Validator.isEmpty(data.company)) {
+    errors.company = 'Company name is required';
+  }
+
+  // Validate from
+  if (Validator.isEmpty(data.from)) {
+    errors.from = 'From date is required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
