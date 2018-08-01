58bb174563bb0f7fc2ed11fcbdea22aa7332a237
Christopher Dennis <cdennis.aus@gmail.com>
2018-07-31 22:15:24 -0500

    Created redux login action

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,6 @@
+import isEmpty from '../validation/isEmpty';
+import { SET_CURRENT_USER } from '../actions/types';
+
 const initialState = {
   isAuthenticated: false,
   user: {}
@@ -5,11 +8,12 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
-    // case TEST_DISPATCH:
-    //   return {
-    //     ...state,
-    //     user: action.payload
-    //   };
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload
+      };
 
     default:
       return state;
