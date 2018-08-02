2ff73a2910f197c4f4c4a7557341405f1f64e469
Christopher Dennis <cdennis.aus@gmail.com>
2018-08-02 16:56:36 -0500

    Created CreateProfile component

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Login from './components/auth/Login';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
 import './App.css';
 
 // Check for token
@@ -51,6 +52,7 @@ class App extends Component {
               <Route exact path='/login' component={Login} />
               <Switch>
                 <PrivateRoute exact path='/dashboard' component={Dashboard} />
+                <PrivateRoute exact path='/create-profile' component={CreateProfile} />
               </Switch>
             </div>
             <Footer />
