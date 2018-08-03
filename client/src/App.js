d66c785f3149e1f54ef9e70dc947139d442a73bf
Christopher Dennis <cdennis.aus@gmail.com>
2018-08-03 18:09:06 -0500

    Created AddExperience component with form

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import Footer from './components/layout/Footer';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
 import './App.css';
 
 // Check for token
@@ -55,6 +56,7 @@ class App extends Component {
                 <PrivateRoute exact path='/dashboard' component={Dashboard} />
                 <PrivateRoute exact path='/create-profile' component={CreateProfile} />
                 <PrivateRoute exact path='/edit-profile' component={EditProfile} />
+                <PrivateRoute exact path='/add-experience' component={AddExperience} />
               </Switch>
             </div>
             <Footer />
