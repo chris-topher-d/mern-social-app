1b359b5d08c55b160dc12f24e806096fcbca879a
Christopher Dennis <cdennis.aus@gmail.com>
2018-08-03 17:27:08 -0500

    Created EditProfile component

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
+import EditProfile from './components/edit-profile/EditProfile';
 import './App.css';
 
 // Check for token
@@ -53,6 +54,7 @@ class App extends Component {
               <Switch>
                 <PrivateRoute exact path='/dashboard' component={Dashboard} />
                 <PrivateRoute exact path='/create-profile' component={CreateProfile} />
+                <PrivateRoute exact path='/edit-profile' component={EditProfile} />
               </Switch>
             </div>
             <Footer />
