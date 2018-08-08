3d9674ea00471fbce4d2b01b2151ae0904118dd9
Christopher Dennis <cdennis.aus@gmail.com>
2018-08-08 16:31:33 -0500

    Created Post component and getPost action

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-import { ADD_POST, GET_POSTS, POST_LOADING, DELETE_POST } from '../actions/types';
+import { ADD_POST, GET_POSTS, GET_POST, POST_LOADING, DELETE_POST } from '../actions/types';
 
 const initialState ={
   posts: [],
@@ -21,6 +21,13 @@ export default function(state = initialState, action) {
         loading: false
       };
 
+    case GET_POST:
+      return {
+        ...state,
+        post: action.payload,
+        loading: false
+      };
+      
     case ADD_POST:
       return {
         ...state,
